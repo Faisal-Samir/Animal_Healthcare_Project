4b149d72b9de88fef01984e92739cda4535c53d1
Faisal-Samir <faisal.samir.riad@gmail.com>
2023-02-09 02:01:23 +0600

    validation added

--- src/Customer/customerform.dto.ts
+++ src/Customer/customerform.dto.ts
@@ -1,10 +1,28 @@
-
+import { IsEmail, IsInt, IsNotEmpty, IsString, Length, Max, Min} from "class-validator";
 export class CustomerRegistration{
+    @IsNotEmpty({message : "please provide name"})
+    @Length(8, 12 , {message : "please name length provide between 8 to 12 character."})
+    @IsString({message : "only string is required for name. "})
     name : string;
+
+    @IsNotEmpty({message : "please provide id"})
+    @IsInt({message : "provide id as a integer number"},)
+    @Min(1,{message : "Id must be greater than or equal 1"})
     id : number;
+
+    @IsNotEmpty({message : "please provide email"})
+    @Length(18, 32 , {message : "please email provide between 18 to 32 character."})
+    @IsEmail()
     email : string;
-    phone : string;
+
+    // @IsNotEmpty({message : "please provide phone number"})
+    // @Min(11,{message : "phone number must be 11 digit"})
+    // @Max(11,{message : "phone number must be 11 digit"})
+    // @IsNotEmpty({message : "please provide password"})
     password : string;
+
+    // @IsNotEmpty({message : "please provide gender"})
+    // @IsString({message : "gender will be string "})
     gender : string;
     address : string;
     city : string; 
