13608de2b9f38d7f7e7d17cfdaceee78f890f1e7
Mubtasim Ihtisham <mubtsaim26@gmail.com>
2023-02-10 22:27:18 +0600

    update

--- src/Pet Shopper/petshopper.service.ts
+++ src/Pet Shopper/petshopper.service.ts
@@ -8,4 +8,33 @@ export class PetShopperService {
 getIndex():string { 
     return "Petshopper Index"; 
 }
+
+
+insertUser(petshopperdto: PetShopperForm): any{
+  return 'Pet shopper Inserted name: ' + petshopperdto.name + ' and id is ' + petshopperdto.id;
+}
+getuserbyid(petshopperdto: PetShopperForm): any{
+  return 'Pet shopper Get name: ' + petshopperdto.name + ' and id is ' + petshopperdto.id;
+}
+updateuser(name: string, id: number): any {
+  return 'Petshopper updated name: ' + name + ' and id is ' + id;
+}
+deleteuser(name:string,id:number):any {
+  return 'Petshopper Name deleted:'+name + 'and id is'+id;
+}
+postproducts(name:string,id:number):any{
+  return 'Post products name : '+name + 'and id is '+id;
+}
+medicinelist(petshopperdto:PetShopperForm):any{
+  return 'Show Medicines : '+ petshopperdto.name+'and id is'+petshopperdto.id;
+}
+foodlist(petshopperdto:PetShopperForm):any{
+  return 'Show Medicines : '+ petshopperdto.name+'and id is'+petshopperdto.id;
+}
+elementslist(petshopperdto:PetShopperForm):any{
+  return 'Show Medicines : '+ petshopperdto.name+'and id is'+petshopperdto.id;
+}
+
+
+
 }
--- src/Pet Shopper/petshopperform.dto.ts
+++ src/Pet Shopper/petshopperform.dto.ts
@@ -2,3 +2,8 @@ export class PetShopperForm {
     id: number;
     name: string;
 }
+export class PetshopperBlog {
+    id: number;
+    title: string;
+    description: string;
+  }
